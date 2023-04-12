41e8135dc33b6a918ff8fed5c6d87c7278f2ad37
Viktor Stenquist <viktor.stenquist@live.se>
2023-04-12 10:07:41 +0200

    first push

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hello man");
+
+//variabler
+
+let greeting = "Hej från en variabel";
+console.log(greeting);
+greeting = 3 ;
+console.log(greeting);
+greeting = "Hello";
+const PI ="3.1415";
+
+console.log(greeting +  'PI is ' + PI);
+
+console.log(`${greeting}, PI is: ${PI}`);
+
